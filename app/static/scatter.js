327ca9fd0d129f2a52ff916d634de616e57b7d93
Piotr Derkowski <piotder@gmail.com>
2016-08-13 20:17:24 +0200

    Disabled the scaling of points when zooming.

--- app/static/scatter.js
+++ app/static/scatter.js
@@ -1,103 +1,107 @@
-function getDisplaySize() {
-  var displayWidth = document.getElementById('container').offsetWidth;
-  var displayHeight = document.getElementById('container').offsetHeight;
-  return [+displayWidth, +displayHeight];
-}
+$(document).ready(function() {
+  function setScales() {
+    return function (bounds) {
+      var xm = bounds.xMin;
+      var ym = bounds.yMin;
+      var XM = bounds.xMax;
+      var YM = bounds.yMax;
 
-function getUsableSize(margin) {
-  var displaySize = getDisplaySize();
-  return [displaySize[0] - margin.left - margin.right, displaySize[1] - margin.top - margin.bottom];
-}
+      var boundsWidth = XM - xm;
+      var boundsHeight = YM - ym;
 
-function loadBounds() {
-  return $.getJSON($SCRIPT_ROOT + 'bounds');
-}
+      if (boundsWidth > boundsHeight) {
+        var d = boundsWidth - boundsHeight;
+        ym -= (d / 2);
+        YM += (d / 2);
+      } else {
+        var d = boundsHeight - boundsWidth;
+        xm -= (d / 2);
+        XM += (d / 2);
+      }
 
-function setViewBoxSize(svg) {
-    var minDim = Math.min.apply(Math, getDisplaySize());
-    svg.attr("viewBox", "0 0 "+minDim+" "+minDim);
-}
+      var minDim = Math.min.apply(Math, getUsableSize());
 
-function setScales(margin, scales) {
-  return function (bounds) {
-    var xm = bounds.xMin;
-    var ym = bounds.yMin;
-    var XM = bounds.xMax;
-    var YM = bounds.yMax;
+      x.domain([ xm, XM ])
+        .range([0, minDim]);
 
-    var boundsWidth = XM - xm;
-    var boundsHeight = YM - ym;
+      y.domain([ ym, YM ])
+        .range([minDim, 0]);
 
-    if (boundsWidth > boundsHeight) {
-      var d = boundsWidth - boundsHeight;
-      ym -= (d / 2);
-      YM += (d / 2);
-    } else {
-      var d = boundsHeight - boundsWidth;
-      xm -= (d / 2);
-      XM += (d / 2);
+      return bounds;
     }
-
-    var minDim = Math.min.apply(Math, getUsableSize(margin));
-
-    scales.x.domain([ xm, XM ])
-      .range([0, minDim]);
-
-    scales.y.domain([ ym, YM ])
-      .range([minDim, 0]);
-
-    return bounds;
   }
-}
 
-function loadPoints(bounds) {
-  var xMin = bounds.xMin;
-  var yMin = bounds.yMin;
-  var xMax = bounds.xMax;
-  var yMax = bounds.yMax;
-
-  return $.getJSON($SCRIPT_ROOT + 'points!'+xMin+'!'+yMin+'!'+xMax+'!'+yMax);
-}
-
-function setPoints(g, scales) {
-  return function (points) {
-    points.forEach(function(p) {
-      p.x = +p.x;
-      p.y = +p.y;
-    });
-
-    return g.selectAll(".dot")
-      .data(points)
-      .enter()
-      .append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(p) { return scales.x(p.x); })
-      .attr("cy", function(p) { return scales.y(p.y); });
+  function getDisplaySize() {
+    var displayWidth = document.getElementById('container').offsetWidth;
+    var displayHeight = document.getElementById('container').offsetHeight;
+    return [+displayWidth, +displayHeight];
   }
-}
 
-function setTip(svg) {
-  return function (dots) {
-    var tip = d3.tip()
-      .attr("class", "d3-tip")
-      .offset([-10, 0])
-      .html(function(p) {
-        return "x: " + p.x + " y: " + p.y;
+  function getUsableSize() {
+    var displaySize = getDisplaySize();
+    return [displaySize[0] - margin.left - margin.right, displaySize[1] - margin.top - margin.bottom];
+  }
+
+  function loadBounds() {
+    return $.getJSON($SCRIPT_ROOT + 'bounds');
+  }
+
+  function loadPoints(bounds) {
+    var xMin = bounds.xMin;
+    var yMin = bounds.yMin;
+    var xMax = bounds.xMax;
+    var yMax = bounds.yMax;
+
+    return $.getJSON($SCRIPT_ROOT + 'points!'+xMin+'!'+yMin+'!'+xMax+'!'+yMax);
+  }
+
+  function setPoints() {
+    return function (points) {
+      points.forEach(function(p) {
+        p.x = +p.x;
+        p.y = +p.y;
       });
 
-    svg.call(tip);
-
-    dots.on("mouseover", tip.show)
-      .on("mouseout", tip.hide);
+      return svgWithMarginAndZoom.selectAll(".dot")
+        .data(points)
+        .enter()
+        .append("circle")
+        .attr("class", "dot")
+        .attr("r", r)
+        .attr("stroke-width", strokeWidth)
+        .attr("cx", function(p) { return x(p.x); })
+        .attr("cy", function(p) { return y(p.y); });
     }
-}
-
-$(document).ready(function() {
-  var scales = {
-    "x": d3.scaleLinear(),
-    "y": d3.scaleLinear()
   }
+
+  function setTip() {
+    return function (dots) {
+      var tip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-10, 0])
+        .html(function(p) {
+          return "x: " + p.x + " y: " + p.y;
+        });
+
+      svg.call(tip);
+
+      dots.on("mouseover", tip.show)
+        .on("mouseout", tip.hide);
+      }
+  }
+
+  function setViewBoxSize() {
+      var minDim = Math.min.apply(Math, getDisplaySize());
+      svg.attr("viewBox", "0 0 "+minDim+" "+minDim);
+  }
+
+  var x = d3.scaleLinear();
+  var y = d3.scaleLinear();
+
+  var margin = { top: 30, right: 30, bottom: 30, left: 30 };
+
+  var r = 3.5;
+  var strokeWidth = 1;
 
   var svg = d3.select("div#container")
     .append("div")
@@ -106,28 +110,31 @@ $(document).ready(function() {
     .attr("preserveAspectRatio", "xMidYMid meet")
     .classed("svg-content-responsive", true);
 
-  setViewBoxSize(svg);
-
-  var margin = { top: 30, right: 30, bottom: 30, left: 30 };
+  setViewBoxSize();
 
   var svgWithMarginAndZoom = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     .append("g");
 
-  var minDim = Math.min.apply(Math, getUsableSize(margin));
+  var minDim = Math.min.apply(Math, getUsableSize());
 
   var zoom = d3.zoom()
     .scaleExtent([1.0, Infinity])
     .translateExtent([[-0.5 * minDim, -0.5 * minDim],[0.5 * minDim, 0.5 * minDim]])
     .on("zoom", function () {
       svgWithMarginAndZoom.attr("transform", d3.event.transform);
+
+      // do not scale dots
+      svgWithMarginAndZoom.selectAll(".dot")
+        .attr("r", r/d3.event.transform.k)
+        .attr("stroke-width", strokeWidth/d3.event.transform.k);
     });
 
   svg.call(zoom);
 
   loadBounds()
-    .then(setScales(margin, scales))
+    .then(setScales())
     .then(loadPoints)
-    .then(setPoints(svgWithMarginAndZoom, scales))
-    .then(setTip(svg));
+    .then(setPoints())
+    .then(setTip());
 });
