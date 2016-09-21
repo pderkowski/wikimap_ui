5d07d9ed5d4a947031a2f2d5f5ef3601ab66c3cc
Piotr Derkowski <piotder@gmail.com>
2016-09-21 18:24:35 +0200

    Added an option to specify the port to run on.

--- run.py
+++ run.py
@@ -5,12 +5,14 @@ import os
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--port', '-p', default=5000, type=int, help="a port used by the app", dest='port')
     parser.add_argument("data_path", help="a path to the data file")
+
     args = parser.parse_args()
 
     if os.path.isfile(args.data_path):
         app = createApp(args.data_path)
-        app.run(debug=True)
+        app.run(debug=False, port=args.port)
     else:
         print 'Could not open "{}".'.format(args.data_path)
 
