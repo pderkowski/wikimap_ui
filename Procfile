b6321ae4cf573a60987b51cf6479d4482aa4658b
Piotr Derkowski <piotder@gmail.com>
2016-09-23 20:50:18 +0200

    Another attempt at starting the app on heroku.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:app --preload
+web: gunicorn --preload -b 0.0.0.0:5000 run:app
--- run.py
+++ run.py
@@ -3,18 +3,21 @@ from app import createApp
 import argparse
 import os
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', '-p', default=5000, type=int, help="a port used by the app", dest='port')
-    parser.add_argument("data_path", help="a path to the data file")
+app = createApp('final')
+app.run(debug=False, host='0.0.0.0', port=5000)
 
-    args = parser.parse_args()
+# def main():
+#     # parser = argparse.ArgumentParser()
+#     # parser.add_argument('--port', '-p', default=5000, type=int, help="a port used by the app", dest='port')
+#     # parser.add_argument("data_path", help="a path to the data file")
 
-    if os.path.isfile(args.data_path):
-        app = createApp(args.data_path)
-        app.run(debug=False, port=args.port)
-    else:
-        print 'Could not open "{}".'.format(args.data_path)
+#     # args = parser.parse_args()
 
-if __name__=="__main__":
-    main()
+#     # if os.path.isfile(args.data_path):
+#     app = createApp('final')
+#     app.run(debug=False, host='0.0.0.0', port=5000)
+#     # else:
+#     #     print 'Could not open "{}".'.format(args.data_path)
+
+# if __name__=="__main__":
+#     main()
