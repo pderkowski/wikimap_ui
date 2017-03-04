00ff272f078cd130ea552ef095aeba1c924706f5
Piotr Derkowski <piotder@gmail.com>
2017-03-04 15:38:47 +0000

    Squashed 'wikimap_ui/models/common/' changes from 4f6d7ac..69fa25a
    69fa25a Added a size field to category table.
    90c6c4f Added search boost for popular articles.
    
    git-subtree-dir: wikimap_ui/models/common
    git-subtree-split: 69fa25a749d395bb301b0f9b9dcdbef3943e9fcd

--- SQLTables.py
+++ SQLTables.py
@@ -63,6 +63,9 @@ class WikimapPointsTable(TableProxy):
     def selectTitles(self):
         return self.select(Query(u"SELECT wp_title FROM wikipoints"))
 
+    def selectTitlesAndRanks(self):
+        return self.select(Query(u"SELECT wp_title, wp_rank FROM wikipoints"))
+
 class WikimapCategoriesTable(TableProxy):
     def __init__(self, tablePath):
         super(WikimapCategoriesTable, self).__init__(tablePath, useCustomTypes=True)
@@ -71,11 +74,12 @@ class WikimapCategoriesTable(TableProxy):
         self.execute(Query(u"""
             CREATE TABLE wikicategories (
                 wc_title        TEXT        NOT NULL,
-                wc_page_ids     LIST        NOT NULL
+                wc_page_ids     LIST        NOT NULL,
+                wc_pages_count  INTEGER     NOT NULL
             );"""))
 
     def populate(self, values):
-        self.executemany(Query(u"INSERT INTO wikicategories VALUES (?,?)", "populating wikicategories table", logStart=True), values)
+        self.executemany(Query(u"INSERT INTO wikicategories VALUES (?,?,?)", "populating wikicategories table", logStart=True), values)
         self.execute(Query(u"CREATE UNIQUE INDEX title_idx ON wikicategories(wc_title);", "creating index title_idx in wikicategories table", logStart=True, logProgress=True))
 
     def selectByTitle(self, title):
